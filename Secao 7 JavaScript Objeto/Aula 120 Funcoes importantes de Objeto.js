1f5e1c925c41d0c58aff182fb23d938284d87a0f
RedSmelter <eduardo2001fevereiro@gmail.com>
2025-01-23 16:57:27 -0300

    aula 121 123 124 125 assistidas

--- Secao 7 JavaScript Objeto/Aula 120 Funcoes importantes de Objeto.js
+++ Secao 7 JavaScript Objeto/Aula 120 Funcoes importantes de Objeto.js
@@ -9,3 +9,12 @@ console.log(Object.values(pessoa))
 console.log(Object.entries(pessoa))
 
 
+
+Object.defineProperty(pessoa, 'dataDeNascimento',{
+    enumerable: true, // Se poderá ser listada nos comandos acima ( keys,values...)
+    writeable: false, // Se poderá ser modificado ou não
+    value : '01/01/2025'
+})
+
+console.log(pessoa)
+console.log(Object.keys(pessoa))
